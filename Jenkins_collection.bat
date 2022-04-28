5406f08fc4f161d9f3305992abae96b3beb36e16
abhikdixit <abhinay.dixit@hotmail.com>
2022-04-28 21:36:20 +0530

    Adding new Project

---
+++ Jenkins_collection.bat
@@ -0,0 +1,2 @@
+cd D:\F Drive\Postman_API_Testing\Newman_Execution_Scripts
+newman run Jsonplaceholder.postman_collection.json --environment  "JSON_PLACE_Holder.postman_environment.json" -r cli,html
